39c43b2e6ae4f9727004fe0c2dd885506ed433b3
Blex <steublex@icloud.com>
2022-05-20 03:36:00 +0200

    main

--- front/js/produit.js
+++ front/js/produit.js
@@ -1,35 +1,24 @@
-produit ()
+const produit = window.location.search.split("?").join("");
+console.table(produit);
 
+let getcanape = [];
 
-async function produit () {
-    const canapeid = getCanapeid
-    const canape = await getCanapes(canapeid)
-    console.table(canapeid)
-    displaycanape(canape)
-}
+const fetchcanape = async () => {
+    await fetch(`http://localhost:3000/api/products/${produit}`)
+    .then((reponse) =>reponse.json())
+    .then((promise) => {
+        getcanape = promise;
+        console.table(getcanape);
+    });
 
-function getCanapeid () {
-    return new URL(location.href).searchParams.get("id")
-}
+};
 
-function getCanapes(canapeid){
-    return fetch(`http://localhost:3000/api/products/${canapeid}`)
-    .then(function(reponse) {
-        return reponse.json()
-    })
-    .then(function(canapes){
-        return canapes
-    })
+const displaycanape = async ()=> {
+    await fetchcanape();
 
-}
-function displaycanape(canape){
-    document.getElementById("photo").innerHTML = ` 
-    <img src="${canape.imgUrl}" alt="Photographie d'un canapé">
+    document.getElementById("photo").innerHTML = `
+    <img src="${getcanape.imageUrl}" alt="Photographie d'un canapé">
     `
-    document.getElementById("title").innerHTML = ` 
-    <h1>${canape.name}<h1>
-    `
-    document.getElementById("price").innerHTML = ` 
-    <p>${canape.price}<p>
-    `
-}
+};
+
+displaycanape();
